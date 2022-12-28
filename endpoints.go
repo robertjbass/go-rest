fdbc7d55c0b6b95edf5c4ba5e99aeea4184e6875
Bob Bass <bob@Bobs-MacBook-Air-M2.local>
2022-12-28 15:41:56 -0500

    got everything working on main package

--- endpoints.go
+++ endpoints.go
@@ -1,4 +1,4 @@
-package endpoints
+package main
 
 import (
 	"github.com/gofiber/fiber/v2"
--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/robertjbass/go-rest/endpoints"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", endpoints.RootHandler)
+	app.Get("/", RootHandler)
 
 	app.Listen(":3000")
 }
